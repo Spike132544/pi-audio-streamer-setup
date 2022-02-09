ad56f722ddd611e6a1afe00ff054fc13dabfd1f3
Stefan Patch <stefan.patch@outlook.com>
2022-02-09 12:19:37 -0800

    Modified paramiko import

--- program.py
+++ program.py
@@ -3,12 +3,21 @@ import sys
 import threading
 import importlib
 
+# Since paramiko may not be imported, try to import it or install it if it isn't installed, then import it.
+try:
+    import paramiko
+except:
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'paramiko'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    globals()['paramiko'] = importlib.import_module('paramiko')
+
 #ip = '192.168.160.188'
 ip = 'raspberrypi.local'
 script_file = 'script.sh'
 
-def install(package):
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+# This function may not be necessary if only paramiko is being 'live-installed'
+# def install(package):    
+#     subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+#     globals()[package] = importlib.import_module(package)
 
 def ssh_update():
     ssh = paramiko.SSHClient()
@@ -42,13 +51,19 @@ def main():
     print('Welcome to the setup program for the RaspberryPi Music Streamer!')
     print("Please don't continue unless you have already flashed Raspbian OS Lite on a >4GB MicroSD Card.")
     print('The easiest way to do this is to use the Raspberry Pi Imager software aavailable at raspberrypi.com/software\n')
+    print('You can input CTRL-C to stop the execution of this program at any time.\n')
+
+    print("First, we'll need your WiFi information to get the Raspberry Pi updating before we continue.")
+    ssid = input('WiFi SSID: ')
+    wifipass = input('WiFi Password: ')
+
+    print('Great! Now please re-insert your microSD card, making sure that it is visible to your computer.\n')
+    # Use of input instead of print to have the program wait for a keypress.
+    input('Press any key to continue.')
 
     #mount /boot
     #copy wifi file, ssh file to /boot
     #modify files on /boot like cmdline.txt and config.txt (wifi file? or template first?)
-
-    install('paramiko') # Paramiko may or may not be installed for the user, so, try to install it first
-    globals()['paramiko'] = importlib.import_module('paramiko')
 
     #make a new thread and run ssh for the update file
     update_thread = threading.Thread(target=ssh_update)
@@ -58,6 +73,7 @@ def main():
 
     update_thread.start()   # Get the pi updating while we're continuing
 
+    # Get User Input and Modify script.sh accordingly
     #get information from user, finish up script.sh, wait for update thread to finish
     print("Yo we're getting information from the user bro")
 
